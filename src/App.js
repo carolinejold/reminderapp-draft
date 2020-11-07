3762c69ecb9b755f25ae3d2e725fffa5eb057635
Caroline Old <carolinejold@gmail.com>
2020-11-07 01:19:48 +0000

    Setting up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [hello, setHello] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      let response = await axios(`http://localhost:5000/`);
+      let result = await response.data.message;
+      setHello(result);
+      console.log(result);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {hello}
+      <p>I am a message on the front end :D </p>
     </div>
   );
 }
