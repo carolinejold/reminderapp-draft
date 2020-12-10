0add4f5894b77aa89fdbdd8feae71a3322efb1de
Caroline Old <carolinejold@gmail.com>
2020-12-10 18:47:46 +0100

    bugs

--- client/src/components/sockets/sockets.ts
+++ client/src/components/sockets/sockets.ts
@@ -1,13 +1,12 @@
 import io from "socket.io-client";
-const server = "window.location.hostname";
+const server = "http://localhost:5000/";
 
 // http://localhost:5000/
 // "https://git.heroku.com/reminders-app-co.git"
 
 const socket = io(server, {
   transports: ["websockets"],
+  upgrade: false
 });
 
 export { socket };
-
-//   upgrade: false,
